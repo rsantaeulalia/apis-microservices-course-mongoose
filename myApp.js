ea652cfcbc54cf54e0030380f212595048e4288d
Rodrigo Santa Eulalia <rsantaeulalia@medallia.com>
2021-05-30 17:42:54 -0300

    add on same faile

--- myApp.js
+++ myApp.js
@@ -3,7 +3,15 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+    name: {type: String, required: true},
+    age:  Number,
+    favoriteFoods: [String]
+})
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
